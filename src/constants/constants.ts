8ac09bdc869559560496c5804910fbd6f82c0a0d
tjd985 <tjd985@gmail.com>
2024-07-29 02:29:49 +0900

    ♻️ [Refactor] 상수 추가
    응답 상수, 에러 상수 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,9 +1,25 @@
 import Constant from "../types/Constant.ts";
 
-const CONSTANTS: Constant = {
+const API_CONSTANTS: Constant = {
   API_URI: "https://bucket-assignment-vercel.vercel.app/api",
   API_LENGTH: "12",
-  API_CATEOGRY: "25",
+  API_CATEGORY: "25",
 };
 
-export default CONSTANTS;
+const RESPONSE_CONSTANTS: Constant = {
+  SUCCESS: "Success",
+  SUCCESS_STATUS: "200",
+};
+
+const ERROR_CONSTANTS: { [key: string]: Constant } = {
+  SERVER_ERROR: {
+    STATUS: "500",
+    MESSAGE: "네트워크 통신이 원활하지 않습니다.",
+  },
+  CLIENT_ERROR: {
+    STATUS: "400",
+    MESSAGE: "올바르지않은 요청 입니다.",
+  },
+};
+
+export { API_CONSTANTS, RESPONSE_CONSTANTS, ERROR_CONSTANTS };
