5a3b75b3583e043c736136944a6d1ed900309f23
tjd985 <tjd985@gmail.com>
2024-07-28 22:24:09 +0900

    ✨ [Feat] products 라우터 구현
    products 라우팅

---
+++ src/routers/products.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+import getProducts from "../controllers/productsController.ts";
+
+const productsRouter = express.Router();
+
+productsRouter.get("/type/:typeString/page/pageNumber", getProducts);
+
+export default productsRouter;
